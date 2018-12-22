e076a8ab6cad623b7d36148f64a264f8b817ec94
Beto <beto@sharplaunch.com>
2018-12-22 11:36:17 +0100

    Proof

---
+++ hamburger.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.hamburger').click(function(e){
+		$menu = $(this).parent();
+		if(!$(this).hasClass('active')) {
+			$(this).addClass('active');
+			$menu.addClass('open');
+		} else {
+			$(this).removeClass('active');
+			$menu.removeClass('open');
+		}
+		e.preventDefault();
+	});
+})
